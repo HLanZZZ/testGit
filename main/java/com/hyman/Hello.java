1f5cce9bad0582d6370af11083b6c3e97e5edcd4
Hyman Zhu <hui_zhu@cathaypacific.com>
2019-09-30 18:03:46 +0800

    add answer

--- main/java/com/hyman/Hello.java
+++ main/java/com/hyman/Hello.java
@@ -2,6 +2,12 @@ package com.hyman;
 
 public class Hello {
     public static void main(String[] args) {
+        Hello hello = new Hello();
         System.out.println("Hello World");
+        hello.print();
+    }
+
+    public void print(){
+        System.out.println("你也好");
     }
 }
